5b63d3316c24696824c9d6ea7167f0c3efa01705
Dustin Moris Gorski <dustinmoris@gmail.com>
2015-08-31 14:00:12 +0100

    Modified About page

--- DustedCodes.Blog/ViewModels/Home/AboutViewModel.cs
+++ DustedCodes.Blog/ViewModels/Home/AboutViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace DustedCodes.Blog.ViewModels.Home
+﻿using System;
+
+namespace DustedCodes.Blog.ViewModels.Home
 {
     public class AboutViewModel : BaseViewModel
     {
+        public static double YearsLivingInLondon
+        {
+            get
+            {
+                var dateMovedToLondon = new DateTime(2012, 10, 16);
+                var timeLivingInLondon = DateTime.Now - dateMovedToLondon;
+                const double daysPerYear = 365.242;
+                return timeLivingInLondon.TotalDays / daysPerYear;
+            }
+        }
     }
 }
