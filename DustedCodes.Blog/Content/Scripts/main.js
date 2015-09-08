1a33b4b86af36c47d83d7e71dccb2135334632cc
Dustin Moris Gorski <dustinmoris@gmail.com>
2015-09-08 22:09:59 +0100

    Added a menu toggle button for mobile views

--- DustedCodes.Blog/Content/Scripts/main.js
+++ DustedCodes.Blog/Content/Scripts/main.js
@@ -3,13 +3,27 @@
 }
 
 (function ($) {
-    dustedcodes = {
+    window.dustedcodes = {
         window: {
             open: function (url, width, height) {
                 width = (typeof width === "undefined") ? 700 : width;
                 height = (typeof height === "undefined") ? 300 : height;
                 window.open(url, "_blank", "width=" + width + ", height=" + height);
             }
+        },
+        menu: {
+            toggle: function () {
+                $("nav").slideToggle("fast", function () {
+                    if ($(this).css("display") === "none") {
+                        $(this).removeClass("nav-expanded").addClass("nav-collapsed").removeAttr("style");
+                    } else {
+                        $(this).removeClass("nav-collapsed").addClass("nav-expanded").removeAttr("style");
+                    }
+                });
+            }
         }
-    }
+    };
+    $(document).on("click", "#nav-toggle-button", function () {
+        dustedcodes.menu.toggle();
+    });
 })(jQuery);
